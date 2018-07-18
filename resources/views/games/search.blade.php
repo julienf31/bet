69e7b73ed783a9a0d46f75afad847bb3b4a96b2f
iMac de Julien <julien.fournier@ynov.com>
2018-07-18 18:25:09 +0200

    push

--- resources/views/games/search.blade.php
+++ resources/views/games/search.blade.php
@@ -27,7 +27,6 @@
                                 <h4 class="media-heading user_name"><small>{!! ($game->privacy)? '<i class="fa fa-lock fa-fw"></i>':'<i class="fa fa-unlock fa-fw"></i>' !!}</small> {{ $game->name }}</h4>
                                 {{ $game->description }}
                                 <p><small><a href="{{ route('games.show', $game->id) }}">Accéder</a> - <a href="{{ route('games.edit', $game->id) }}">Paramétres</a></small></p>
-                                <a href="{{ route('bet',$game->id) }}" class="btn btn-success">Parier</a>
                             </div>
                         </div>
                     @endforeach
