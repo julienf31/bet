1362085c91562157f1492f05a748b1cfdbea02ae
iMac de Julien <julien.fournier@ynov.com>
2018-08-23 20:20:07 +0200

    leaderboard parts

--- app/Game.php
+++ app/Game.php
@@ -57,51 +57,8 @@ class Game extends Model
 
     public function getStyledRank()
     {
+        $rank = $this->ranking();
 
-        $rank = array();
-        foreach ($this->participants as $participant) {
-            $part = array();
-            $user = User::where('id', $participant->user_id)->first();
-            $bets = User::find($participant->user_id)->bets()->where('game_id', $this->id)->get();
-            $part['name'] = $user->firstname;
-            $part['lastname'] = $user->lastname;
-            $part['pseudo'] = $user->pseudo;
-            $part['id'] = $user->id;
-            $part['score'] = 0;
-            foreach ($bets as $bet) {
-                $match = $bet->match()->first();
-                if ($bet->result) {
-                    $part['score'] += 1;
-                } else {
-                    //bet fail
-                }
-            }
-            array_push($rank, $part);
-        }
-        $rank = array_reverse(array_sort($rank, function ($value) {
-            return $value['score'];
-        }));
-
-        $return = '<table class="table tab-pane"><tr><th style="width: 20px;"></th><th>Pseudo</th><th>Score</th></tr>';
-        foreach ($rank as $i => $r) {
-            switch ($i){
-                case 0:
-                    $star = '<i class="fa fa-star text-yellow"></i>';
-                    break;
-                case 1:
-                    $star = '<i class="fa fa-star text-gray"></i>';
-                    break;
-                case 2:
-                    $star = '<i class="fa fa-star text-brown"></i>';
-                    break;
-                default:
-                    $star = '';
-            }
-            $return = $return.'<tr><td>'.$star.'</td>';
-            $return = $return.'<td><a href="'.route('profile', $r['id']).'" data-toggle="tooltip" title="'.$r['pseudo'].'">'.$r['name'].' '.strtoupper(substr($r['lastname'],0,1)).'</a></td><td>'.$r['score'].'</td></tr>';
-        }
-        $return = $return.'</table>';
-
-        return $return;
+        return view('games.parts.leaderboard',compact('rank'));
     }
 }
---
+++ resources/views/games/parts/leaderboard.blade.php
@@ -0,0 +1,20 @@
+<table class="table tab-pane">
+    <tr>
+        <th style="width: 20px;"></th>
+        <th>Pseudo</th>
+        <th>Score</th>
+    </tr>
+    @foreach($rank as $i => $r)
+        <tr>
+            <td>
+                {!! ($i == 0)? '<i class="fa fa-star text-yellow"></i>':(($i == 1)? '<i class="fa fa-star text-gray"></i>':(($i == 2)? '<i class="fa fa-star text-brown"></i>':'')) !!}
+            </td>
+            <td>
+                <a href="{{ route('profile', $r['id']) }}" data-toggle="tooltip" title="{{ $r['pseudo'] }}">
+                    {{ $r['name'] }} {{ strtoupper(substr($r['lastname'],0,1)) }}.
+                </a>
+            </td>
+            <td>{{ $r['score'] }}</td>
+        </tr>
+    @endforeach
+</table>
